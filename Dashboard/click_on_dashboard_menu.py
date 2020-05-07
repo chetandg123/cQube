e54107c73954c499f8921455d91e4fcde92921f5
chetan.goudar@tibilsolutions.com <sdevaraja85@gmail.com>
2020-05-07 21:07:58 +0530

    first commit

---
+++ Dashboard/click_on_dashboard_menu.py
@@ -0,0 +1,29 @@
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.select import Select
+from Data.Paramters import Data
+
+
+class DashboardMenu(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        self.driver = webdriver.Chrome(Data.Path)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        self.driver.get(Data.URL)
+        print(self.driver.title)
+        self.driver.find_element_by_xpath(Data.email).send_keys(Data.username)
+        self.driver.find_element_by_xpath(Data.pwd).send_keys(Data.password)
+        self.driver.find_element_by_xpath(Data.loginbtn).click()
+        time.sleep(10)
+
+    def test_click_on_menu(self):
+        self.driver.find_element_by_xpath(Data.Dashboard).click()
+    def tearDown(self):
+        time.sleep(3)
+        self.driver.close()
+if __name__ == "__main__":
+    unittest.main()
+
